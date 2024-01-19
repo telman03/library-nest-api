15b39b6b6eb9b382e929d9be056d8cf8a90aa1a5
telman03 <telmangadimov1@gmail.com>
2024-01-19 18:36:49 +0400

    Add NestExpressApplication to NestFactory.create()

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import {SwaggerModule, DocumentBuilder} from '@nestjs/swagger';
 import * as cookieParser from 'cookie-parser';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     cors: { 
       credentials: true,
     },
